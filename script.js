4d7f9e0a3dabe98f22cd2aec3e04031c1b9dd8c4
esmail sarwari <esmail.sarwari10@gmail.com>
2024-10-07 23:44:15 -0700

    form validation added

--- script.js
+++ script.js
@@ -122,7 +122,41 @@ class App {
     }
 
     _newWorkOut(e) {
+        const formValidation = (...inputs) =>
+            inputs.every((inp) => Number.isFinite(inp));
+        const allPositive = (...inputs) => inputs.every((inp) => inp > 0);
+
         e.preventDefault();
+
+        // get the data from the form
+        // check if the data is valid
+        // if workout running, create running object
+        // if workout cycling, create cycling object
+        // add new objects to workout array
+        // render workout on map as marker
+        const type = inputType.value;
+        const distance = +inputDistance.value;
+        const duration = +inputDuration.value;
+
+        if (type === 'running') {
+            const cadence = +inputCadence.value;
+
+            if (
+                !formValidation(distance, duration, cadence) ||
+                !allPositive(distance, duration, cadence)
+            )
+                return alert('Please enter a valid positive number');
+        }
+
+        if (type === 'cycling') {
+            const elevation = +inputElevation.value;
+
+            if (
+                !formValidation(distance, duration, elevation) ||
+                !allPositive(distance, duration)
+            )
+                return alert('Please enter a valid positive number');
+        }
 
         // empty the input values
         inputType.value =
