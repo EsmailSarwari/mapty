4a486f32a28796705590b336bf26e2657c3f4d9f
esmail sarwari <esmail.sarwari10@gmail.com>
2024-10-07 23:19:54 -0700

    workout, running, cycling classes created

--- script.js
+++ script.js
@@ -11,6 +11,42 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout {
+    date = new Date();
+    id = (Date.now() + '').slice(-10);
+
+    constructor(coords, distance, duration) {
+        this.coords = coords;
+        this.distance = distance; // in km
+        this.duration = duration; // in mind
+    }
+}
+
+class Running extends Workout {
+    constructor(coords, distance, duration, candence) {
+        super(coords, distance, duration);
+        this.candence = candence;
+        this.clacPace();
+    }
+
+    clacPace() {
+        this.pace = this.duration / this.distance;
+        return this.pace;
+    }
+}
+
+class Cycling extends Workout {
+    constructor(coords, distance, duration, elevationGain) {
+        super(coords, distance, duration);
+        this.elevationGain = elevationGain;
+        this.calcSpeed();
+    }
+
+    calcSpeed() {
+        this.speed = this.distance / this.duration;
+    }
+}
+
 class App {
     #map;
     #mapEvent;
