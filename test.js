54187dc4644e68bbfb5a4f2f4813dfa0a775ccae
esmail sarwari <esmail.sarwari10@gmail.com>
2024-10-17 22:05:31 -0700

    project completed

--- test.js
+++
@@ -1,7 +0,0 @@
-const date =  Date.now();
-const number = '123456789';
-// console.log(number.toString().slice(-1));
-console.log(typeof(number));
-
-// const newdate = new Date();
-// console.log(newdate)
